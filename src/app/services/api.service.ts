d17350865e93eb1dbea3cee1704927c2135fec68
UnCompa <brandonddxd@gmail.com>
2024-10-03 19:45:56 -0500

    fix: :recycle: Se intenta solucionar problema que la api no se renderiza en tiempo real

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,15 +1,27 @@
-import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { inject, Injectable, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  private baseUrl = "https://whenisthenextmcufilm.com/api"
-
-  private http = inject(HttpClient)
+  private baseUrl = "https://whenisthenextmcufilm.com/api";
+  private http = inject(HttpClient);
+  private platformId = inject(PLATFORM_ID);
 
   getMovie<T>() {
-    return this.http.get<T>(this.baseUrl)
+    const headers = new HttpHeaders({
+      'Cache-Control': 'no-cache',
+      'Pragma': 'no-cache',
+      'Expires': '0'
+    });
+
+    if (isPlatformBrowser(this.platformId)) {
+      return this.http.get<T>(this.baseUrl, { headers });
+    } else {
+      // Manejo de solicitudes en el servidor
+      return this.http.get<T>(this.baseUrl, { headers });
+    }
   }
 }
